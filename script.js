7445ebe75f6b701cc813701e864fa6f47cd004b4
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-05-05 11:59:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -23,3 +23,68 @@ $("ul li").css("fontSize", "20px");
 
 
 //$(".orderedList").text("Hello");
+
+$(".btn").click(function(){
+    alert('You clicked button');
+    $(this).css("backgroundColor", "grey");
+});
+
+
+$(".textInput").keypress(function(event){
+    if(event.which === 13){
+        console.log("You have pressed enter");
+    }
+    
+});
+
+ $(".textInput").on("click", function(){
+    $(this).css("backgroundColor","violet");
+ });
+
+ $(".btnClear").on("click", function(){
+    $(".textInput").css("backgroundColor","white");
+ });
+
+ $(".btnClear").on("mouseover", function(){
+    $(this).css("backgroundColor","violet");
+ });
+
+ $(".btnClear").on("mouseleave", function(){
+    $(this).css("backgroundColor","black");
+ });
+
+
+/*
+
+ $(".catImg").on("click", function(){
+    $(this).fadeOut(1000, function(){
+      console.log("Fade Out");
+    });
+ });
+
+ */
+ 
+
+
+ $(".ImgFadeIn").on("click", function(){
+   $(".catImg").fadeToggle(2000, function(){
+      console.log("Toggled");
+   });
+ });
+
+
+ $("#slideUp").on("click", function(){
+   $(this).slideUp(1000);
+ });
+ 
+ /*
+
+ $("#slideDown").on("click", function(){
+   $("#slideUp").slideDown(2000);
+ });
+
+ */
+
+ $("#slideDown").on("click", function(){
+   $("#slideUp").slideToggle(2000);
+ });
